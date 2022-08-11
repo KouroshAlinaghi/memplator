8da47eff03cfc439f40a501a4ecfb5879ef54c72
Kourosh Alinaghi <kouroshalinaghi@gmail.com>
2022-08-11 20:58:54 +0430

    dry (dont repeat yourself)

--- index.js
+++ index.js
@@ -16,12 +16,19 @@ await db.read()
 
 const bot = new TelegramBot(token, {polling: true})
 
+/**
+ * @param {string} query
+ */
 const search = query => {
   return db.data.filter(post => {
     return post.text.match(new RegExp(query, 'i'))
   })
 }
 
+/**
+ * @param {string} id - if event is error, id is the error message
+ * @param {string} event
+ */
 const informMe = (id, event) => {
   let resp = ''
   switch (event) {
@@ -34,80 +41,93 @@ const informMe = (id, event) => {
     case 'deletePost':
       resp = `Deleted #${id} from the database.`
       break;
-    case 'fail':
+    case 'error':
       resp = `We got a problem: ${id}` // id is now the error message ha ha hahahhahaheg
-    default: 
-      resp = "what the fuck"
   }
 
   bot.sendMessage('@' + helperChannelId, resp)
 }
 
-async function handlePost(msg) {
-  db.data.push({
-    id: msg.message_id,
-    text: msg.caption || '',
-    height: msg.photo.at(-1).height,
-    width: msg.photo.at(-1).width
-  })
+/**
+ * @param {string} event 
+ * @param {Object} msg 
+ * @param {string} match - is null unless in deletePost event
+ */
+async function handle(event, msg, match) {
+  try {
+    switch (event) {
 
-  await db.write()
-}
+      case "newPost": 
+        db.data.push({
+          id: msg.message_id,
+          text: msg.caption || '',
+          height: msg.photo.at(-1).height,
+          width: msg.photo.at(-1).width
+        })
+        await db.write()
+        informMe(msg.message_id, event)
+        break
 
-async function handleEdit(msg) {
-  const index = db.data.findIndex(post => {
-    return post.id == msg.message_id
-  })
+      case "editPost":
+        const index = db.data.findIndex(post => {
+          return post.id == msg.message_id
+        })
+        if (index >= 0) {
+          db.data[index].text = msg.caption
+          await db.write()
+        } else {
+          throw "Editing a non-existing post."
+        }
+        informMe(msg.message_id, event)
+        break
 
-  if (index >= 0) {
-    db.data[index].text = msg.caption
-    await db.write()
+      case "deletePost":
+        const updatedData = db.data.filter(p => {return p.id !== parseInt(match[1])})
+        if (updatedData.length == db.data.length) {
+          throw `Post #${msg.message_id} does not exist.`
+        }
+        db.data = updatedData
+        await db.write()
+        informMe(match[1], event)
+        break
+        
+      case "inlineQuery":
+        const results = search(msg.query).map(r => {
+          return {
+            id: r.id,
+            type: 'photo',
+            photo_url: `https://t.me/${channelId}/${r.id}`,
+            thumb_url: `https://t.me/${channelId}/${r.id}`,
+            photo_height: r.height,
+            photo_width: r.width
+          }
+        }).slice(0, 50)
+
+        bot.answerInlineQuery(msg.id, results)
+    }
+  } catch(error) {
+    informMe(error, "error")
   }
-}
-
-async function handleDelete(msg, match) {
-  db.data = db.data.filter(post => {return post.id !== parseInt(match[1])})
-  await db.write()
 }
 
 bot.on('channel_post', msg => {
   if (msg.photo && msg.sender_chat.username == channelId) {
-    handlePost(msg).then(
-    () => informMe(msg.message_id, 'newPost'),
-    err => informMe(err, 'fail')
-  )
+    handle("newPost", msg, null)
   }
 })
 
 bot.on('edited_channel_post_caption', msg => {
   if (msg.photo && msg.sender_chat.username == channelId) {
-    handleEdit(msg).then(
-    () => informMe(msg.message_id, 'editPost'),
-    err => informMe(err, 'fail')
-  )
+    handle("editPost", msg, null)
   }
 })
 
 bot.onText(/\/delete (.+)/, (msg, match) => {
   if (msg.from.username == myUsername) {
-    handleDelete(msg, match).then(
-    () => informMe(match[1], 'deletePost'),
-    err => informMe(err, 'fail')
-  )
+    handle("deletePost", msg, match)
   }
 })
 
 bot.on('inline_query', msg => {
-  const results = search(msg.query).map(r => {
-    return {
-      id: r.id,
-      type: 'photo',
-      photo_url: `https://t.me/${channel_id}/${r.id}`,
-      thumb_url: `https://t.me/${channel_id}/${r.id}`,
-      photo_height: r.height,
-      photo_width: r.width
-    }
-  }).slice(0, 50)
-
-  bot.answerInlineQuery(msg.id, results)
+  handle("inlineQuery", msg, null)
 })
